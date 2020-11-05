337a57e1c837a8483102d172a1b7a78cce272768
FesenkoMaks <maks.fesenko.1995@gmail.com>
2020-11-05 14:32:03 +0300

    Updates

---
+++ precache-manifest.6b4e6862abdab7491c862964f53796e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b86384d1e02fb241ebc9ba5c93283a",
+    "url": "/Todolist-TS/index.html"
+  },
+  {
+    "revision": "c7160be2e5d989744faf",
+    "url": "/Todolist-TS/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "68f01cc52c42af795a17",
+    "url": "/Todolist-TS/static/js/2.bd1f8b1f.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist-TS/static/js/2.bd1f8b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7160be2e5d989744faf",
+    "url": "/Todolist-TS/static/js/main.23039847.chunk.js"
+  },
+  {
+    "revision": "8e1d9570a61c4111829b",
+    "url": "/Todolist-TS/static/js/runtime-main.9e872e3d.js"
+  }
+]);
